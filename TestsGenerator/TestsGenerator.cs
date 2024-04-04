8525a38ffe518917393fe6f987a41370ab96ca55
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-04-04 13:29:54 +0300

    test generation implementation in process

--- TestsGenerator/TestsGenerator.cs
+++ TestsGenerator/TestsGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace TestsGenerator
 {
@@ -23,7 +26,21 @@ namespace TestsGenerator
     {
         public static TestWrapper[] GenerateTests(string inputSource)
         {
-            return null;
+            List<TestWrapper> tests = new List<TestWrapper>();
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(inputSource);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+            var classes = root.DescendantNodes(n => !(n is ClassDeclarationSyntax)).OfType <ClassDeclarationSyntax>();
+            foreach ( ClassDeclarationSyntax classDeclaration in classes)
+            {
+                generateSingleTestFile(classDeclaration, root, tests);
+            }
+            return [.. tests];
+        }
+
+        private static string generateSingleTestFile(SyntaxNode classNode, CompilationUnitSyntax root, 
+            List<TestWrapper> tests)
+        {
+            return "";
         }
     }
 }
